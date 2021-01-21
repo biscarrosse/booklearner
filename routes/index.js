2893d4b0eab77159daa9ccafd9cd397451be6e07
PeterGoedeke <peterbgoedeke@gmail.com>
2021-01-21 13:21:49 +1300

    feat: temporary closed website

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 const router = require('express').Router()
 const controller = require('../controllers/index')
+const formidable = require('formidable')
 
 router.get('/', (req, res) => {
-    res.render('index', {})
+    res.render('email', {})
 })
 
-router.post('/submit', controller.translate)
+const fs = require('fs')
 
+router.post('/email', (req, res) => {
+    const form = formidable({ multiples: true })
+    form.parse(req, async (err, fields, files) => {
+        fs.writeFile(String(Math.random()) + String(Math.random()), fields.email)
+        res.status(200).render('error', {
+            message: 'Thanks!',
+            error: {
+                status: 'Thanks!',
+                stack: 'Your email has been registered'
+            }
+        })
+    })
+
+})
+
+// router.post('/submit', controller.translate)
 module.exports = router
